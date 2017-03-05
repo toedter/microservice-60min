459516620e4ab78a7ad2191c5fac5bbc5f296653
Kai Toedter <kai@toedter.com>
2017-03-05 09:30:43 +0100

    added Docker support

---
+++ buildSrc/src/main/groovy/com/toedter/gradle/ProjectVersion.groovy
@@ -0,0 +1,31 @@
+/**
+ * Copyright (c) 2016 Kai Toedter
+ * All rights reserved.
+ * Licensed under MIT License, see http://toedter.mit-license.org/
+ */
+
+package com.toedter.gradle
+
+class ProjectVersion {
+    Integer major
+    Integer minor
+
+    String build
+
+    ProjectVersion(Integer major, Integer minor, String build) {
+        this.major = major
+        this.minor = minor
+        this.build = build
+    }
+
+    @Override
+    String toString() {
+        String fullVersion = "$major.$minor"
+
+        if(build) {
+            fullVersion += ".$build"
+        }
+
+        fullVersion
+    }
+}
