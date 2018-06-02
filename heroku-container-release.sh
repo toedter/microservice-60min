b4da68b9899a8a8f0869637f91da223b845970d4
Kai Toedter <kai@toedter.com>
2018-06-02 12:37:41 +0200

    chore: (hopefully) fixed docker image id for Heroku release travis-ci build

--- heroku-container-release.sh
+++ heroku-container-release.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 imageId= docker inspect registry.heroku.com/microservice-60min/web --format={{.Id}}
 curl -n -X PATCH https://api.heroku.com/apps/microservice-60min/formation \
--d '{"updates":[{"type":"web","docker_image":"$imageId"}]}' \
+-d '{"updates":[{"type":"web","docker_image":"'"$imageId"'"}]}' \
 -H "Content-Type: application/json" \
 -H "Accept: application/vnd.heroku+json; version=3.docker-releases" \
--H "Authorization: Bearer eb305d9e-75ff-4881-acbc-325c28efb3ae"
+-H "Authorization: Bearer $HEROKU_AUTH_TOKEN"
