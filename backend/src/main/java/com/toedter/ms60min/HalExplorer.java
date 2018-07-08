665e8487fb4e7d2bae1bc6e9df7ceb4e9278a322
Kai Toedter <kai@toedter.com>
2018-07-08 14:38:31 +0200

    feat: redirecting get requests with html media type to hal explorer

---
+++ backend/src/main/java/com/toedter/ms60min/HalExplorer.java
@@ -0,0 +1,19 @@
+package com.toedter.ms60min;
+
+import org.springframework.data.rest.webmvc.BasePathAwareController;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.View;
+import org.springframework.web.servlet.view.RedirectView;
+
+import javax.servlet.http.HttpServletRequest;
+
+@BasePathAwareController
+public class HalExplorer {
+	@RequestMapping(value = { "/", "" }, method = RequestMethod.GET, produces = MediaType.TEXT_HTML_VALUE)
+	public View index(HttpServletRequest request) {
+        final String url = "/webjars/hal-explorer/0.9.4/index.html?#url=/api";
+        return new RedirectView(url);
+	}
+}
