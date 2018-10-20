c132ed47c07836d0f64785423a262ac6c13cf3ce
Kai Toedter <kai@toedter.com>
2018-10-20 12:32:02 +0200

    chore: updated to Angular 7 and Spring Boot 2.0.6

--- backend/src/main/java/com/toedter/ms60min/thing/Thing.java
+++ backend/src/main/java/com/toedter/ms60min/thing/Thing.java
@@ -2,6 +2,7 @@ package com.toedter.ms60min.thing;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -9,6 +10,7 @@ import javax.persistence.Id;
 
 @Data
 @Entity
+@NoArgsConstructor
 public class Thing {
     @Id @GeneratedValue @JsonIgnore
     private Long id;
